0287b63c7c7a259b66b00d746c6d16962733d837
SkyLiNer823 <flamesky823@gmail.com>
2021-09-17 12:22:27 +0800

    Big update

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const row = document.getElementById("row")
 const col = document.getElementById("col")
+const difficult = document.getElementById("difficult")
+const life = document.getElementById("life")
 const confirmBtn = document.getElementById("confirmBtn")
-const board = document.getElementById('board')
-const rowHintboard = document.getElementById('rowHintboard')
-const colHintboard = document.getElementById('colHintboard')
+const Board = document.getElementById('Board')
 
 
 function colCount(rowNum, colNum, ansArray) {
-    colHintboard.innerHTML = ''
     for (let i = 0; i < colNum; i++) {
         let numArray = new Array();
         let colArray = new Array();
@@ -38,12 +37,10 @@ function colCount(rowNum, colNum, ansArray) {
         for (let k = 0; k < numArray.length; k++) {
             document.getElementById(`Col${i+1}`).innerText += String(numArray[k]) + ".";
         }
-        colHintboard.innerHTML += `<br>Col${i+1}: ${document.getElementById(`Col${i+1}`).innerText}</br>`
     }
 }
 
 function rowCount(rowNum, colNum, ansArray) {
-    rowHintboard.innerHTML = ''
     for (let i = 0; i < rowNum; i++) {
         let numArray = new Array();
         count = 0;
@@ -69,9 +66,8 @@ function rowCount(rowNum, colNum, ansArray) {
             numArray.push(0);
         }
         for (let k = 0; k < numArray.length; k++) {
-            document.getElementById(`Row${i+1}`).innerText += String(numArray[k])+".";
+            document.getElementById(`Row${i+1}`).innerText += String(numArray[k]) + ".";
         }
-        rowHintboard.innerHTML+=`<br>Row${i+1}: ${document.getElementById(`Row${i+1}`).innerText}</br>`
     }
 
 }
@@ -95,35 +91,52 @@ function check(rowNum, colNum, ansArray) {
 confirmBtn.addEventListener("click", function() {
     rowNum = parseInt(row.value);
     colNum = parseInt(col.value);
-    board.innerHTML = ''
+    difficultNum = parseInt(difficult.value);
+    lifeNum = parseInt(life.value);
+
+    if (lifeNum < 1)
+        lifeNum = 1;
+    if (difficultNum < 1)
+        difficultNum = 1;
+    else if (difficultNum > 5)
+        difficultNum = 5;
+
+    document.getElementById("nowLife").innerText = lifeNum;
+    let nowlife = document.getElementById("nowLife").innerText
+
+    Board.innerHTML = ''
     for (let i = 0; i < rowNum + 1; i++) {
         for (let j = 0; j < colNum + 1; j++) {
             if (i == 0 && j == 0) {
-                board.innerHTML += `<button class="hiddenBtn"></button>`;
+                Board.innerHTML += `<button class="hiddenBtn"></button>`;
             } else if (i == 0) {
-                board.innerHTML += `<button class="hintBtn" id="Col${j}"></button>`;
+                Board.innerHTML += `<button class="hintBtn" id="Col${j}"></button>`;
             } else if (j == 0) {
-                board.innerHTML += `<button class="hintBtn" id="Row${i}"></button>`;
+                Board.innerHTML += `<button class="hintBtn" id="Row${i}"></button>`;
             } else {
-                board.innerHTML += `<button class="pressBtn" id="${i}x${j}"></button>`;
+                Board.innerHTML += `<button class="pressBtn" id="${i}x${j}"></button>`;
             }
         }
     }
+
     let ansArray = new Array(); //先宣告一維
     for (let i = 0; i < rowNum; i++) {
         ansArray[i] = new Array(); //宣告二維
         for (let j = 0; j < colNum; j++) {
             let randNum = Math.floor(Math.random() * 10);
-            if (randNum <= 7)
+            if (randNum <= 10 - difficultNum)
                 ansArray[i][j] = 1;
             else
                 ansArray[i][j] = 0;
         }
     }
+
     rowCount(rowNum, colNum, ansArray);
     colCount(rowNum, colNum, ansArray);
+
     let pressBtn = document.getElementsByClassName("pressBtn");
     let hintBtn = document.getElementsByClassName("hintBtn");
+
     for (let i = 0; i < pressBtn.length; i++) {
         pressBtn[i].addEventListener("click", function() {
             const btnRow = Math.floor(i / colNum);
@@ -136,14 +149,24 @@ confirmBtn.addEventListener("click", function() {
                 if (this.style.backgroundColor != "black") {
                     this.style.backgroundColor = "red";
                     this.innerHTML = "X"
+                    nowlife -= 1;
+                    document.getElementById("nowLife").innerText = nowlife;
+                    if (nowlife == 0) {
+                        alert("您的生命值已歸0!\n遊戲將重置");
+                        confirmBtn.click();
+                    }
+
+
                 }
             }
         })
     }
+
     for (let i = 0; i < hintBtn.length; i++) {
         hintBtn[i].addEventListener("click", function() {
             alert(hintBtn[i].innerText);
         })
     }
-    board.style.cssText = `width: ${(colNum+1)*50}px; height: ${(rowNum+1)*50}px;`;
+
+    Board.style.cssText = `width: ${(colNum+1)*50}px; height: ${(rowNum+1)*50}px;`;
 })
